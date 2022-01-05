18e22070a48f9bd6a5880df511bb7dca0d6a92f8
oduodg <werewildfire@gmail.com>
2022-01-05 11:34:17 +0900

    프로그래머스: 같은 숫자는 싫어, 다른 아이디어 추가

--- Programmers/같은 숫자는 싫어.py
+++ Programmers/같은 숫자는 싫어.py
@@ -7,9 +7,20 @@ def solution(arr):
             answer.append(num)
     return answer
 
+def solution2(arr):
+    answer = []
+    for num in arr:
+        if answer[-1:] == [num]:
+            continue
+        answer.append(num)
+    return answer
+
 # Test Case
 arr1 = [1,1,3,3,0,1,1]
 arr2 = [4,4,4,3,3]
 
 print(solution(arr1))
 print(solution(arr2))
+
+print(solution2(arr1))
+print(solution2(arr2))
