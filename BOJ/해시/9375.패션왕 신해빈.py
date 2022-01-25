7e38f0a0748c7f7e90b261d238b89e367502076f
oduodg <werewildfire@gmail.com>
2022-01-25 10:44:39 +0900

    solved BOJ: 🥈Ⅲ - 9375.패션왕 신혜빈

---
+++ BOJ/해시/9375.패션왕 신해빈.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline # 입력 가속
+t = int(input()) # 테스트 케이스의 수
+for _ in range(t):
+    n = int(input()) # 의상의 수
+    clothes = {}
+    for _ in range(n):
+        name, type = map(str, input().split()) # 의상의 이름과 종류
+        if type not in clothes:
+            clothes[type] = 2
+        else:
+            clothes[type] += 1
+    print(clothes)
+    answer = 1
+    for key, value in clothes.items():
+        answer *= value
+    print(answer-1)
