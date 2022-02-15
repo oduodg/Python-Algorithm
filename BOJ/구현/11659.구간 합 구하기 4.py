ef6d802db1263ca1e293432b5e3e8c88d97cd161
oduodg <werewildfire@gmail.com>
2022-02-15 15:08:11 +0900

    solved BOJ: 🥈Ⅲ - 11659.구간 합 구하기 4

--- BOJ/구현/11659.구간 합 구하기 4.py
+++ BOJ/구현/11659.구간 합 구하기 4.py
@@ -9,7 +9,7 @@ for _ in range(m):
 """
 sec = [0, nums[0]] + [0] * (n-1)
 for i in range(1, n):
-    sec[i+1] = sec[i] + nums[i]  # 구간의 합을 미리 구해놓는다.
+    sec[i+1] = sec[i] + nums[i]  # 처음부터 해당 숫자까지의 구간의 합을 미리 구해놓는다.
 for _ in range(m):
     start, end = map(int, input().split())
-    print(sec[end]-sec[start-1])
+    print(sec[end]-sec[start-1])  # 끝 구간에서 start 직전까지의 구간의 합을 빼준다.
