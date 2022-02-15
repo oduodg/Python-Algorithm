d4115c2d5fcac13652189d74abd489c6cf9a042d
oduodg <werewildfire@gmail.com>
2022-02-15 15:06:15 +0900

    solved BOJ: 🥈Ⅰ - 11659.구간 합 구하기 4

---
+++ BOJ/구현/11659.구간 합 구하기 4.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+""" # 시간 초과
+for _ in range(m):
+    start, end = map(int, input().split())
+    print(sum(nums[start-1:end]))
+"""
+sec = [0, nums[0]] + [0] * (n-1)
+for i in range(1, n):
+    sec[i+1] = sec[i] + nums[i]  # 구간의 합을 미리 구해놓는다.
+for _ in range(m):
+    start, end = map(int, input().split())
+    print(sec[end]-sec[start-1])
